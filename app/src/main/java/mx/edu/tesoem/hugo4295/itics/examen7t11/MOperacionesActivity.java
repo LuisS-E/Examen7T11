59ec3145e862354c9ba539d2b216d78118a7cf47
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2019-04-04 15:19:54 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/hugo4295/itics/examen7t11/MOperacionesActivity.java
@@ -0,0 +1,35 @@
+package mx.edu.tesoem.hugo4295.itics.examen7t11;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MOperacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moperaciones);
+    }
+
+    public void suma(View view) {
+        Intent intent = new Intent(this, OSumaActivity.class);
+        startActivity(intent);
+    }
+
+    public void resta(View view) {
+        Intent intent = new Intent(this, ORestaActivity.class);
+        startActivity(intent);
+    }
+
+    public void potencia(View view) {
+        Intent intent = new Intent(this, OPotenciaActivity.class);
+        startActivity(intent);
+    }
+
+    public void salir(View view) {
+        Intent intent = new Intent(this, SalidaActivity.class);
+        startActivity(intent);
+    }
+}
